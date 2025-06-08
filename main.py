74295a1b7d354457115a1a3d4c389121cad4db0c
EatMyShortsBruh <eatmyshortsbrotato@gmail.com>
2025-06-07 21:49:45 -0400

    add journal entry text box

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import tkinter as tk
 window = tk.Tk()
 window.title("My Journal")
 window.geometry("400x300")
+
+entry_box = tk.Text(window, height=10, width=40)
+entry_box.pack(pady=20)
+
 window.mainloop()
